b5e72ebe45ee35608a15c15a499d63c875c0d8a1
lucascustodioCATOLICA <142275808+lucascustodioCATOLICA@users.noreply.github.com>
2023-08-22 20:29:03 -0300

    Update main.c

--- 5/main.c
+++ 5/main.c
@@ -7,7 +7,7 @@ int main() {
     printf("Informe o valor em Reais: R$");
     scanf("%f", &num1);
     
-    real_para_dolar = num1 * DOLAR;
+    real_para_dolar = num1 / DOLAR;
     printf("Valor em Dolar: %.2f \n", real_para_dolar);
 
     return 0;
